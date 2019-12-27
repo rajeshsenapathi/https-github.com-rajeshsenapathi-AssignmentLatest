fc3ab04b5320ce5b79bc6980014d537d480b0415
senapathi rajesh <senapathi.rajesh@infosys.com>
2019-12-27 15:16:15 +0530

    adding refresh control

--- Assignment/Extensions/Extensions.swift
+++ Assignment/Extensions/Extensions.swift
@@ -47,6 +47,15 @@ extension ViewController: UITableViewDelegate,UITableViewDataSource {
         alertController.preferredAction = RetryAction
         self.present(alertController, animated: true, completion: nil)
     }
+    func presentAlert(withTitle title: String, message : String) {
+           let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+           let OKAction = UIAlertAction(title: "OK", style: .default) { action in
+               print("You've pressed OK Button")
+           }
+           alertController.addAction(OKAction)
+           self.present(alertController, animated: true, completion: nil)
+       }
+       
     
 }
  
--- Assignment/Model/ViewControllers/ViewController.swift
+++ Assignment/Model/ViewControllers/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var jsonRowsArray: [Rows]?
      var handler = ApiHandler()
+    private let refreshControl = UIRefreshControl()
     let  canadaTableView: UITableView = {
         let t = UITableView()
         t.translatesAutoresizingMaskIntoConstraints = false
@@ -31,19 +32,35 @@ class ViewController: UIViewController {
               canadaTableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
                canadaTableView.tableFooterView = UIView()
               canadaTableView.estimatedRowHeight = UITableView.automaticDimension
-        
+         refreshControl.tintColor = UIColor(red:0.25, green:0.72, blue:0.85, alpha:1.0)
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 0.25, green: 0.72, blue: 0.85, alpha: 1.0)]
+       let attributedTitle = NSAttributedString(string: "Pull Down To Refresh", attributes: attributes)
+        refreshControl.attributedTitle = attributedTitle
+        refreshControl.addTarget(self, action: #selector(refreshdata(_:)), for: .valueChanged)
+           if #available(iOS 10.0, *) {
+               canadaTableView.refreshControl = refreshControl
+           } else {
+               canadaTableView.addSubview(refreshControl)
+           }
+       
          if Reachability.isConnectedToNetwork(){
-                 self.getItemsList()
             print(Constants.InternetConnectivity.NETWORK_SUCCESS_MSG)
+            self.presentAlert(withTitle: "", message: "Pull down Table View to Refresh the data")
                }else{
                    print(Constants.InternetConnectivity.NETWORK_Failure_MSG)
-                   
             self.presentNetowrkAlertWithTwoButton(withTitle: Constants.AlertConstatnts.TITLE, message: Constants.AlertConstatnts.Title_Msg) { (UiAlertActionIn) in
                 self.getItemsList()
             }
             return;
                }
+    
     }
+    @objc private func refreshdata(_ sender: Any) {
+        // Fetch Weather Data
+        self.getItemsList()
+        self.refreshControl.endRefreshing()
+    }
+
     
     
     func getItemsList(){
@@ -72,4 +89,9 @@ class ViewController: UIViewController {
     }
   
 }
-
+extension ViewController{
+    @objc func refreshData(_ refreshControl: UIRefreshControl) {
+        print("refreshing")
+        canadaTableView.endUpdates()
+    }
+}
